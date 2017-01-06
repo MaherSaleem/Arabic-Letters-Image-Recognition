559f07bdb8ce685a80eaf0849e36a76ceaaaa704
MaherSaleem <the-maher@live.com>
2017-01-06 11:31:35 +0200

    store in pickle file
    matching still not working

--- main.py
+++ main.py
@@ -99,37 +99,58 @@ def getTrainingImagesPaths(Model="Model(B)",Font = "Simplified Arabic"):
     newpath = ROOT_DIR + str("\image dataset\PAC-NF\\") + str(Model) + '\\' + str(Font) + '\\'
     #newpath = "C:\\Users\Maher\Desktop\PAC-NF\PAC-NF\\" + str(Model) + '\\' + str(Font) + '\\'
 
-    paths = []
+    pathsAlph = []
 
-    for i in range(1, 2):
+
+    for i in range(1, 30):
+        pathsForOneChar = []
+
         newpath2 = str(newpath) + str(i) + '\\'
         for j in range(1, 5):
+
             newpath3 = str(newpath2) + str(j) + '\\'
             # print(newpath3)
+            imageIndex=2
             if os.path.exists(newpath3):
                 newpath4 = str(newpath3) + "n\\"
-                newpath5 = str(newpath4) + "2.png"
-                paths.append(newpath5)
-    return paths
+
+                while(1):
+                    newpath5 = str(newpath4) + str(imageIndex) + ".png"
+                    img = cv2.imread(newpath5, 0)
+                    if(img.shape[0] > 30 or img.shape[1]>30):
+                        imageIndex+=1
+                    else:
+                        break
+
+                pathsForOneChar.append(newpath5)
+        pathsAlph.append(pathsForOneChar)
+    return pathsAlph
 
 def getSubImageData(img , part):
     x1 ,x2 , y1,y2 =  part['x'][0] , part['x'][1]  , part["y"][0] , part["y"][1]
+    # cv2.imshow('im', img[x1:x2, y1:y2])
+    # cv2.waitKey(0)
+
     return img[x1:x2,y1:y2]
 
 def pickle_keypoints(keypoints, descriptors):
     i = 0
     temp_array = []
     for point in keypoints:
-        temp = (point.pt, point.size, point.angle, point.response, point.octave,
-        point.class_id, descriptors[i])
-        ++i
+        temp = [point.pt, point.size, point.angle, point.response, point.octave, point.class_id, descriptors[i]]
+        i+=1
         temp_array.append(temp)
     return temp_array
 
-def unpickle_keypoints(array):
+
+"""
+This code unpickels for 1 shape(1 image)
+"""
+def unpickle_keypoints(shape):
+
     total = []
     try:
-        for sub in array:
+        for sub in shape:
             # if sub is None:
             #     continue
             keypoints = []
@@ -139,42 +160,128 @@ def unpickle_keypoints(array):
                 temp_descriptor = point[6]
                 keypoints.append(temp_feature)
                 descriptors.append(temp_descriptor)
-            total.append((keypoints, np.array(descriptors)))
+            total.append([keypoints, descriptors])
     except:
         pass
     return total
 
+def storeToFile(fileName ,temp_array):
+    cPickle.dump(temp_array, open(fileName, "wb"))
+
+
+def constructTrainingArray():
+    trainingDataPaths = getTrainingImagesPaths()
+    temp_array = []
+    i = 0
+    for eachChar in trainingDataPaths:
+        j = 0
+        listForChar = []
+        for eachShape in eachChar:
+            listForShape = []
+            img = cv2.imread(eachShape, 0)
+            # cv2.imshow('img'+str(i),img)
+            # cv2.waitKey(0)
+            paddedImage = addPadding(img, horizontalPadding, verticalPadding)
+            # cv2.imshow('img'+str(i+1),paddedImage)
+            # cv2.waitKey(0)
+
+            # n = 3
+            # m = 3
+            parts = divideImage(paddedImage, n, m)
+            ImagePartsKeyPoints_array = []
+            for part in parts:
+                subImage = getSubImageData(paddedImage, part)
+                # Initiate SIFT detector
+                sift = cv2.xfeatures2d.SIFT_create()
+                kp1, des1 = sift.detectAndCompute(subImage, None)
+                temp = pickle_keypoints(kp1, des1)
+                listForShape.append(temp)
+                # listForShape.append([kp1,des1])
+                # Initiate ORB detector
+                # orb = cv2.ORB_create()
+                # find the keypoints and descriptors with ORB
+                # kp1, des1 = orb.detectAndCompute(subImage, None)
+            listForChar.append(listForShape)
+        temp_array.append(listForChar)
+    # print(temp_array[0][1])
+    storeToFile("keypoints.p"  , temp_array)
 
 
 
-trainingDataPaths = getTrainingImagesPaths()
-temp_array = []
-for path in trainingDataPaths:
-     img = cv2.imread(path , 0)
-     paddedImage = addPadding(img , 50 , 50)
-     n = 3
-     m = 3
-     parts = divideImage(paddedImage , n,m)
-     temp2_array = []
-     for part in parts:
-        subImage = getSubImageData(paddedImage , part)
+
+
+
+# print(des1)
+print("="*300)
+
+# constructTrainingArray()
+
+def getBestMatching(path="test2.png"):
+    img = cv2.imread(path, 0)
+    cv2.imshow('img',img)
+    cv2.waitKey(0)
+    paddedImage = addPadding(img, horizontalPadding, verticalPadding)
+    # cv2.imshow('img',paddedImage)
+    # cv2.waitKey(0)
+
+    keypoints_database = cPickle.load(open("keypoints.p", "rb"))
+
+    parts = divideImage(paddedImage, n, m)
+    ImagePartsKeyPoints_array = []
+    i = 0
+    for part in parts:
+        print("part is ", i)
+        subImage = getSubImageData(paddedImage, part)
+        cv2.imshow('img2', subImage)
+        cv2.waitKey(0)
+
         # Initiate SIFT detector
         sift = cv2.xfeatures2d.SIFT_create()
         kp1, des1 = sift.detectAndCompute(subImage, None)
-        temp = pickle_keypoints(kp1, des1)
-        temp2_array.append(temp)
-        # Initiate ORB detector
-       # orb = cv2.ORB_create()
-        # find the keypoints and descriptors with ORB
-        #kp1, des1 = orb.detectAndCompute(subImage, None)
+        print(des1)
+        print(des1)
+        #finding the best match between the input image and the training data
 
-     temp_array.append(temp2_array)
-    # print(temp_array)
+        # create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-        #rint(kp1)
-       # print(des1)
-cPickle.dump(temp_array, open("keypoints_database.p", "wb"))
-keypoints_database = cPickle.load(open("keypoints_database.p", "rb"))
-#print(keypoints_database[1])
-kp1, desc1 = unpickle_keypoints(keypoints_database[1])
+        # Match descriptors.
+        for charIndex,eachChar in enumerate(keypoints_database):
+            for shapeIndex,eachShape in enumerate(eachChar):
+                # print(charIndex,shapeIndex)
+                shapePartsKeypoints = unpickle_keypoints(keypoints_database[charIndex][shapeIndex])
+                # print("length is ",len(shapePartsKeypoints))
+                kp2,des2 = shapePartsKeypoints[i]
+                # print(des2)
+                # print(kp2,des2)
+                matches = bf.match(des1, des2)
+                # Sort them in the order of their distance.
+                matches = sorted(matches, key=lambda x: x.distance)
+                # print(charIndex , shapeIndex,matches)
+        i += 1
+
+
+
+
+
+
+
+
+n = 3
+m = 3
+horizontalPadding = 30
+verticalPadding = 30
+
+getBestMatching()
+# keypoints_database = cPickle.load(open("keypoints.p", "rb"))
+# print(keypoints_database)
+
+# print(keypoints_database[1][2])
+# t = unpickle_keypoints(keypoints_database[1][2])
+# print("+++++++++++++\n"  ,t)
+# print(keypoints_database[1])
+# tot =[]
+# tot =  unpickle_keypoints(keypoints_database[1])
+# print(tot)
+#kp1, desc1 = unpickle_keypoints(keypoints_database[1])
 # print(len(unpickle_keypoints(keypoints_database[0])))
